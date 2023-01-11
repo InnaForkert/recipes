ae90aa0beb695e0e7f4c3f581c31727e3b7f7011
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-11 14:16:47 +0200

    check for no search results

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom";
 import "./RecipeList.css";
 
 function RecipeList({ recipes }) {
+  if (recipes.length === 0) {
+    return <div className="error">No recipes to load...</div>;
+  }
+
   return (
     <div className="recipe-list">
       {recipes.map(({ id, title, cookingTime, method }) => (
