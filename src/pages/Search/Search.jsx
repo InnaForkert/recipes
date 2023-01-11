f389a36c8eaaebf04f386e0f7a3f9cbdd8d38677
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-11 14:13:55 +0200

    search results

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,7 +1,24 @@
+import { useLocation } from "react-router-dom";
 import "./Search.css";
+import { useFetch } from "../../hooks/useFetch";
+import RecipeList from "../../components/RecipeList/RecipeList";
 
 function Search() {
-  return <></>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const url = "http://localhost:3000/recipes?q=" + query;
+  const { error, isPending, data } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page--title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
 
 export default Search;
