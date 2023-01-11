31cd168b0c07e5844f34809d30ae596a7d381c43
InnaForkert <110421964+InnaForkert@users.noreply.github.com>
2023-01-11 03:01:48 +0200

    create recipe form component

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,5 +1,50 @@
 import "./Create.css";
+import { useState } from "react";
 
 export default function Create() {
-  return <></>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  }
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
